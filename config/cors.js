b63881633b3ab5bfc829c17e9cb7567c088aa710
Albert Martin <albert@imor.tl>
2020-12-06 15:33:30 -0600

    chore: add config for cors policy

---
+++ config/cors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  allowHeaders: [
+    'Accept',
+    'Access-Control-Allow-Origin',
+    'ApolloGraphQL-Client-Name',
+    'ApolloGraphQL-Client-Version',
+    'Authorization',
+    'Content-Type',
+    'X-Requested-With',
+    'X-HTTP-Method-Override'
+  ]
+}
