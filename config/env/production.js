9ae027733f024f920cf0096f825f242bf55123b3
Albert Martin <albert@imor.tl>
2020-12-12 12:43:41 -0600

    chore: add release tracking in sentry

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,6 @@
 module.exports = {
   sentry: {
-    dsn: 'https://857b280e679d4e85ba3494ef9002f914@o490225.ingest.sentry.io/5554117'
+    dsn: 'https://857b280e679d4e85ba3494ef9002f914@o490225.ingest.sentry.io/5554117',
+    release: process.env.VERCEL_GIT_COMMIT_SHA
   }
 }
